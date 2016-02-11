90c3d6e34fab5812e42a5a03b066c61285acd055
The-Duchess <iarcher@pdx.edu>
2016-02-10 23:13:45 -0800

    Update example.rb

--- example.rb
+++ example.rb
@@ -3,8 +3,8 @@ require 'rFlex'
 def main
 	lexertemp = LexicalAnalyzer.new(ARGV[0].to_s, ARGV[1].to_s)
 
-	lexertemp.lexers.each do |lexer|
-			lexer.do :lex do |token, regexp, type|
+	lexertemp.lexers.each do |lexerT|
+			lexerT.do :lex do |token, regexp, type|
 				if type != "ignore"
 					puts "lexeme: #{type} | value: #{token}"
 					puts "-------~-----~--~------~---~-------~----~------------~---~------------~-----"
